2300fc1d89c6024ccee69fe519c2895a2cb0ea57
Mark Harris <mark_harris77@hotmail.com>
2020-07-13 19:15:15 +0100

    Created Project Model and added some more html

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    description = models.CharField(max_length=256)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title
+
